35fd12041d1e21ee8895ca18b56e111778aef6d5
EOussama <oussama_gtacj@live.fr>
2023-11-28 17:11:07 +0100

    Exercice 2, question 2, 3, 4

--- src/exercice_2.sql
+++ src/exercice_2.sql
@@ -2,3 +2,13 @@
 
 -- 1 - Écrire l'instruction qui permet d'afficher le contenu de la SGA.
 SELECT * FROM V$SGA;
+
+-- 2 - Changer la taille de la SGA å 900M.
+ALTER SYSTEM SET SGA_TARGET = 900M SCOPE=MEMORY;
+
+-- 3 - Quelle est la vue qui permet d'accéder aux données du dictionary cache.
+-- V$ROWCACHE.
+SELECT * FROM V$ROWCACHE;
+
+-- 4 - Donner Ia requéte SQL qui permet de vérifier s'il faut, ou pas, optimiser le dictionary cache.
+Select SUM(GETS) "Somme des gets", SUM(GETMISSES) "Somme des getmisses" FROM V$rowcache;
