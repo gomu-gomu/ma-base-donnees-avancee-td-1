8866fe80fc720157b1b191e8b25bf3b626829814
EOussama <oussama_gtacj@live.fr>
2023-11-28 21:03:56 +0100

    Exercice 9 and 10.

--- src/exercice_2.sql
+++ src/exercice_2.sql
@@ -10,7 +10,7 @@ ALTER SYSTEM SET SGA_TARGET = 900M SCOPE=MEMORY;
 -- V$ROWCACHE.
 SELECT * FROM V$ROWCACHE;
 
--- 4 - Donner Ia requéte SQL qui permet de vérifier s'il faut, ou pas, optimiser le dictionary cache.
+-- 4 - Donner la requéte SQL qui permet de vérifier s'il faut, ou pas, optimiser le dictionary cache.
 Select SUM(GETS) "Somme des gets", SUM(GETMISSES) "Somme des getmisses" FROM V$rowcache;
 
 -- 5 - Calculer le rapport R, est ce que cette mémoire est insuffisante dans ce cas.
@@ -21,7 +21,7 @@ Select SUM(GETMISSES) / (SUM(GETS) + Sum(getmisses)) * 100 "R" FROM V$rowcache;
 -- 6 - Ecrire la requéte SQL qui permet de modifier SPA = 80M.
 ALTER SYSTEM SET SHARED_POOL_SIZE = 80M;
 
--- 7 - Donner la requéte SQL qui permet de vérifier s'il faut augmenter Ia taille du Library cache.
+-- 7 - Donner la requéte SQL qui permet de vérifier s'il faut augmenter la taille du Library cache.
 Select SUM(RELOADS) / (SUM(RELOADS) + Sum(PINS)) * 100 "R" FROM V$librarycache;
 -- Reloads: Nombre de demandes infructueuses
 -- Pins : Nombre d’exécutions sans défaut de cache
@@ -29,3 +29,11 @@ Select SUM(RELOADS) / (SUM(RELOADS) + Sum(PINS)) * 100 "R" FROM V$librarycache;
 
 -- 8 - Sachant que : somme des pins = 45640 et somme des reloads = 270, trouver la valeur de R, que faut-il conclure ?
 Select 45640 / (270 + 45640) * 100 "R" FROM V$librarycache WHERE ROWNUM = 1;
+
+-- 9 - Donner la requéte SQL qui permet de vérifier si la mémoire database buffer cache est suffisante ou pas.
+SELECT (1 - ((SUM(DECODE(NAME, 'physical reads', VALUE, 0)) / ((SUM(DECODE(NAME, 'db block gets', VALUE, 0)) + (SUM(DECODE(NAME, 'consistent gets', VALUE, 0)))))))) * 100 AS "R" FROM V$SYSSTAT;
+-- Si R > 60% ou 70%, OK.
+
+-- 10 - Quelle est la formule qui permet de vérifier si la mémoire data base buffer cache est uffisante ou pas, calculer cette valeur et dites s'il faut augmenter la taille de la SGA
+-- Formule = (1 - (physical reads / (db block gets + consistent gets))) * 100
+SELECT (1 - ((SUM(DECODE(NAME, 'physical reads', VALUE, 0)) / ((SUM(DECODE(NAME, 'db block gets', VALUE, 0)) + (SUM(DECODE(NAME, 'consistent gets', VALUE, 0)))))))) * 100 AS "R" FROM V$SYSSTAT;
